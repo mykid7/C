c6fe82c47fd5537f9810244b427e27dbd3298110
mykid7 <55013563+mykid7@users.noreply.github.com>
2019-09-07 15:29:20 +0900

    Add files via upload

---
+++ 예제2-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>	// stdio : stamulard input output(표준입출력)의 줄임말
+
+/*int main(void)
+{
+	printf("Be happy");		// 문자열 "Be happy" 출력
+	printf("My frined");	// 문자열 "My frined" 출력
+
+	return 0;	//프로그램 종료
+}*/
